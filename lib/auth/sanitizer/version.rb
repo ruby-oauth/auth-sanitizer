21d6165dca7eef8e7b9bb01fdfc4cc8bab4e5b96
Peter H. Boling <peter.boling@gmail.com>
2026-04-06 01:59:17 -0600

    🎉 Tada!

---
+++ lib/auth/sanitizer/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Auth
+  module Sanitizer
+    VERSION = "0.1.0"
+  end
+end
---
+++ spec/auth/sanitizer_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Auth::Sanitizer do
+  it "has a version number" do
+    expect(Auth::Sanitizer::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
