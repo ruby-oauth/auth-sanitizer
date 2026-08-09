608902d948089361f6cd157a0cd36f0106e81d0e
Peter H. Boling <peter.boling@gmail.com>
2026-08-08 20:13:49 -0600

    ⬆️ kettle-family v1.2.43

--- lib/auth/sanitizer.rb
+++ lib/auth/sanitizer.rb
@@ -2,11 +2,11 @@
 
 require "version_gem"
 
+require_relative "sanitizer/version"
 require_relative "sanitizer/thing_filter"
 require_relative "sanitizer/core"
 require_relative "sanitizer/filtered_attributes"
 require_relative "sanitizer/sanitized_logger"
-require_relative "sanitizer/version"
 
 Auth::Sanitizer::Version.class_eval do
   extend VersionGem::Basic
